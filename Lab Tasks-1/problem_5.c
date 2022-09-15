a7fe99dd330cc67453568f84a7da776383820bfe
Kazi Jawad <112785366+Kazi-Jawad-10@users.noreply.github.com>
2022-09-15 22:58:53 +0600

    Add files via upload

---
+++ Lab Tasks-1/problem_5.c
@@ -0,0 +1,37 @@
+/*The length & breadth of a rectangle and radius of a circle are input through the keyboard.
+Write a program to calculate the area & perimeter of the rectangle, and the area & circumference
+of the circle.
+Author: Kazi Jawad */
+
+
+#include<stdio.h>
+
+int main()
+{
+    float l, b, r,ca, cc, ra, rp;
+
+
+    printf("Enter the length of rectangle: ");
+    scanf("%f", &l);
+    printf("Enter the breadth of rectangle: ");
+    scanf("%f", &b);
+
+
+    printf("Enter the radius of circle: ");
+    scanf("%f", &r);
+
+
+     ra = l * b;
+     rp = 2 * (l + b);
+
+
+     ca = 3.14 * r * r;
+     cc = 2 * 3.14 * r;
+
+     printf("\nThe area of the rectangle: %0.2f", ra);
+     printf("\nThe perimeter of the rectangle: %.2f", rp);
+     printf("\n\nThe area of the circle: %.2f", ca);
+     printf("\nThe circumference of the circle: %.2f", cc);
+
+     return (0);
+}
