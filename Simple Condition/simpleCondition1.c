2178299f7e9ca3ed612ee88d964450f3733abe27
Kazi Jawad <112785366+Kazi-Jawad-10@users.noreply.github.com>
2022-09-17 06:34:23 +0600

    Add files via upload

---
+++ Simple Condition/simpleCondition1.c
@@ -0,0 +1,40 @@
+/*If cost price and selling price of an item is input through the keyboard, write aprogram to
+determine whether the seller has made profit or incurred loss. Also determine how much profit
+he made or loss he incurred
+
+Author: Kazi Jawad */
+
+
+#include<stdio.h>
+
+int main()
+{
+    float cost_price, selling_price, profit, loss;
+
+    printf("Enter the cost price of an item: ");
+    scanf("%f", &cost_price);
+
+    printf("Enter the selling price of an item: ");
+    scanf("%f", &selling_price);
+
+    if (selling_price > cost_price)
+    {
+
+        profit = selling_price - cost_price;
+        printf("We earn %.2f profit by selling item.", profit);
+
+    }
+
+    else if (selling_price < cost_price)   //here we get loss on selling item
+    {
+
+        loss = cost_price - selling_price;
+        printf("\nWe incurred %.2f loss on selling item.", loss);
+    }
+
+    else
+    {
+        printf("\nWe don't get any loss and profit on selling item");
+    }
+
+}
